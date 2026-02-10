dc21a065614f070d3f191780cd8fc093bb92507d
Asilbek2706 <249135167+Asilbek2706@users.noreply.github.com>
2026-02-10 14:01:44 +0500

    update filterations

--- src/components/appFilter/appFilter.jsx
+++ src/components/appFilter/appFilter.jsx
@@ -4,7 +4,10 @@ const AppFilter = ({ updateFilterHandler, filter }) => {
   return (
     <div className="btn-group">
         {btnsArr.map(btn => (
-            <button key={btn.name} onClick={() => updateFilterHandler(btn.name)} className={`btn ${ filter === btn.name ? "btn-dark" : "btn-outline-dark "}`} type="button">
+            <button key={btn.name}
+                    onClick={() => updateFilterHandler(btn.name)}
+                    className={`btn ${ filter === btn.name ? "btn-dark" : "btn-outline-dark "}`}
+                    type="button">
                 {btn.label}
             </button>
         ))}
