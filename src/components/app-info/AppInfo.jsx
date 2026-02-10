65550189cbb541a6494a2c10265c19c47e259d42
Asilbek2706 <249135167+Asilbek2706@users.noreply.github.com>
2026-02-10 20:29:14 +0500

    Finish the project

--- src/components/app-info/AppInfo.jsx
+++ src/components/app-info/AppInfo.jsx
@@ -1,9 +1,9 @@
-import './AppInfo.css';
+import './AppInfo.scss';
 
 const AppInfo = ({ allMoviesCount, favouriteMovieCount }) => {
   return (
     <div className="app-info">
-      <p className="fs-3 text-uppercase">Barcha kinolar soni: {allMoviesCount}</p>
+      <p className='fs-3 text-uppercase'>Barcha kinolar soni: {allMoviesCount}</p>
       <p className="fs-4 text-uppercase">Sevimli film: {favouriteMovieCount}</p>
     </div>
   );
